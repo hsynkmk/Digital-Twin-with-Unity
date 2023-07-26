421301b6ddb2aecfbe3f7efd0895e9ee9d71b82e
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-26 03:19:10 +0300

    BuildManager: Comments

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -3,12 +3,12 @@ using UnityEngine.EventSystems;
 
 public class BuildManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] buildObjects;
-    private GameObject selectedObject;
-    private GameObject previewObject;
-    private bool isPreviewing;
-    private Ray ray;
-    private RaycastHit hit;
+    [SerializeField] GameObject[] buildObjects; // array of objects to build
+    private GameObject selectedObject; // the currently selected object
+    private GameObject previewObject; // the preview object
+    private bool isPreviewing; // whether or not the preview object is active
+    private Ray ray; // the ray to cast from the camera to the mouse position
+    private RaycastHit hit; // the object that was hit by the ray
 
     private void Update()
     {
@@ -20,13 +20,14 @@ public class BuildManager : MonoBehaviour
         }
     }
 
+    // Handle the previewing of the selected object
     private void HandlePreview()
     {
         if (isPreviewing)
         {
             if (Input.GetMouseButtonDown(1))
             {
-                Destroy(previewObject);
+                DestroyPreviewObject();
                 isPreviewing = false;
             }
             else
@@ -36,8 +37,8 @@ public class BuildManager : MonoBehaviour
                 if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Ground"))
                 {
                     MovePreviewObject(hit.point);
-
                 }
+
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     RotatePreviewObject();
@@ -46,16 +47,19 @@ public class BuildManager : MonoBehaviour
         }
     }
 
+    // Move the preview object to the target position
     private void MovePreviewObject(Vector3 targetPosition)
     {
         previewObject.transform.position = targetPosition;
     }
 
+    // Rotate the preview object by 90 degrees
     private void RotatePreviewObject()
     {
         previewObject.transform.Rotate(Vector3.up, 90f);
     }
 
+    // Place the selected object in the scene at the target position
     private void PlaceObject()
     {
         if (previewObject != null)
@@ -72,11 +76,13 @@ public class BuildManager : MonoBehaviour
         }
     }
 
+    // Instantiate the selected object at the given position
     private void InstantiateSelectedObject(Vector3 position)
     {
         Instantiate(selectedObject, position, Quaternion.identity);
     }
 
+    // Select an object from the build menu
     public void SelectObject(int index)
     {
         if (index >= 0 && index < buildObjects.Length)
@@ -89,6 +95,7 @@ public class BuildManager : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(null); // cancel keyboard (pressing space etc.)
     }
 
+    // Destroy the preview object if it exists
     private void DestroyPreviewObject()
     {
         if (previewObject != null)
@@ -97,6 +104,7 @@ public class BuildManager : MonoBehaviour
         }
     }
 
+    // Create a preview object for the selected object
     private void CreatePreviewObject()
     {
         previewObject = Instantiate(selectedObject);
