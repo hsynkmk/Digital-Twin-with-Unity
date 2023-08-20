65bd56d5bdd32e6911a1b87ca0ad12e0511f36d6
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-08-21 01:00:45 +0300

    Going to destinations in order

--- AGV Transport Robots/Assets/Scripts/RobotParent.cs
+++ AGV Transport Robots/Assets/Scripts/RobotParent.cs
@@ -6,8 +6,16 @@ using UnityEngine.AI;
 
 public class RobotManager : MonoBehaviour
 {
+    enum RobotState
+    {
+        OnPark,
+        OnProduct,
+        OnDestination,
+    }
+
     [SerializeField] private Button startButton;
     private List<Transform> robotList = new List<Transform>();
+    private List<RobotState> robotStates = new List<RobotState>();
     private int currentRobotIndex = 0;
 
     private void Start()
@@ -18,6 +26,35 @@ public class RobotManager : MonoBehaviour
         foreach (Transform child in transform)
         {
             robotList.Add(child);
+            robotStates.Add(RobotState.OnPark); // Initialize each robot's state
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < robotStates.Count; i++)
+        {
+            Transform robot = robotList[i];
+            NavMeshAgent agent = robot.GetComponent<NavMeshAgent>();
+            RobotState currentState = robotStates[i];
+
+            switch (currentState)
+            {
+                case RobotState.OnProduct:
+                    if (!agent.pathPending && agent.remainingDistance < 0.1f)
+                    {
+                        MoveRobotToDestination(robot, agent);
+                        robotStates[i] = RobotState.OnDestination; // Update state
+                    }
+                    break;
+                case RobotState.OnDestination:
+                    if (!agent.pathPending && agent.remainingDistance < 0.1f)
+                    {
+                        MoveRobotToPark(robot, agent);
+                        robotStates[i] = RobotState.OnPark; // Reset state for the next cycle
+                    }
+                    break;
+            }
         }
     }
 
@@ -25,9 +62,27 @@ public class RobotManager : MonoBehaviour
     {
         if (currentRobotIndex < robotList.Count)
         {
-            NavMeshAgent agent = robotList[currentRobotIndex].GetComponent<NavMeshAgent>();
-            agent.SetDestination(Product.GetLocation().position);
+            Transform robot = robotList[currentRobotIndex];
+            NavMeshAgent agent = robot.GetComponent<NavMeshAgent>();
+            MoveRobotToProductLocation(robot, agent);
+            robotStates[currentRobotIndex] = RobotState.OnProduct; // Update state
             currentRobotIndex++;
         }
+    }
+
+    // Implement the methods to move the robot to the product and destination locations
+    private void MoveRobotToProductLocation(Transform robot, NavMeshAgent agent)
+    {
+        agent.SetDestination(Product.GetLocation().position);
+    }
+
+    private void MoveRobotToDestination(Transform robot, NavMeshAgent agent)
+    {
+        agent.SetDestination(Destination.GetLocation().position);
+    }
+
+    private void MoveRobotToPark(Transform robot, NavMeshAgent agent)
+    {
+        agent.SetDestination(Park.GetLocation().position);
     }
 }
